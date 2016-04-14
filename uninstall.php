671e2c44cdfcb74d25c1d5e8f685616162d000a0
pete@petenelson.com <petenelson@fortress.local>
2016-04-14 12:04:08 -0500

    updated uninstall

--- uninstall.php
+++ uninstall.php
@@ -3,6 +3,8 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) die( 'restricted access' );
 
 $keys = array(
 	'wp-rest-api-toolbox-general',
+	'wp-rest-api-toolbox-core',
+	'wp-rest-api-toolbox-ssl',
 );
 
 // remove options

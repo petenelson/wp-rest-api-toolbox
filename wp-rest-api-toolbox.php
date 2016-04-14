6157cf7b4e1e4aef9ae6d90452c343040eb03ac5
pete@petenelson.com <petenelson@fortress.local>
2016-04-14 11:10:36 -0500

    initial commit of the wp scaffold plugin

---
+++ wp-rest-api-toolbox.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Wp-rest-api-toolbox
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: wp-rest-api-toolbox
+ * Domain Path: /languages
+ * @package Wp-rest-api-toolbox
+ */

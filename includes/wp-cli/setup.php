71423c09d147cd8d2fa01b644045ff6b7aae4cd9
pete@petenelson.com <petenelson@fortress.local>
2016-04-14 21:49:32 -0500

    added ssl commands

--- includes/wp-cli/setup.php
+++ includes/wp-cli/setup.php
@@ -2,7 +2,8 @@
 
 // our wp-cli commands
 $includes = array(
-	'class-rest-api-toolbox-rest-api-command.php'
+	'class-rest-api-toolbox-rest-api-command.php',
+	'class-rest-api-toolbox-ssl-command.php'
 	);
 
 foreach ( $includes as $include ) {

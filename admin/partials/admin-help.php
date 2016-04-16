76a58fefff151e772f7137fc7f232164b3db761b
Pete Nelson <pete@petenelson.com>
2016-04-16 12:36:54 -0500

    added badges to readme

--- admin/partials/admin-help.php
+++ admin/partials/admin-help.php
@@ -8,6 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) die( 'restricted access' );
 	<p>
 		<?php esc_html_e( 'E-Mail', 'rest-api-toolbox' ) ?>: <a href="mailto:pete@petenelson.com">pete@petenelson.com</a><br/>
 		<?php esc_html_e( 'Twitter', 'rest-api-toolbox' ) ?>: <a href="https://twitter.com/GunGeekATX" target="_blank">@GunGeekATX</a><br/>
+		<?php esc_html_e( 'WordPress Repo', 'rest-api-toolbox' ) ?>: <a href="https://wordpress.org/plugins/rest-api-toolbox/" target="_blank">https://wordpress.org/plugins/rest-api-toolbox/</a><br/>
 		<?php esc_html_e( 'GitHub', 'rest-api-toolbox' ) ?>: <a href="https://github.com/petenelson/wp-rest-api-toolbox" target="_blank">https://github.com/petenelson/wp-rest-api-toolbox</a><br/>
 	</p>
 
